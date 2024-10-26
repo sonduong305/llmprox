19380ca7ce1f610f8c19a1ae624604bc698dc68d
Son Duong <son.duong@eastagile.com>
2024-10-26 16:28:27 +0700

    chore: Update setting prod

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,7 +45,7 @@ LOCALE_PATHS = [str(BASE_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {"default": env.db("POSTGRES_URL")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
