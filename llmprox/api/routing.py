46b6402fbd1d70d32dc2688c4b5a68bd8c97789d
Son Duong <son.duong@eastagile.com>
2024-10-27 00:15:55 +0700

    chore: clean up project

--- llmprox/api/routing.py
+++
@@ -1,8 +0,0 @@
-# llm_app/routing.py
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/api/v1/completion/$", consumers.LLMCompletionConsumer.as_asgi()),
-]
